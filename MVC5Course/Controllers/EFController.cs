5d45f6b1fcc695e7d981b1eac874307f0b00d19a
ycwu <ycwu@NB-00021>
2016-10-16 11:19:21 +0800

    17 練習透過 Entity Framework 查詢資料、建立資料、刪除資料、取出單筆資料

---
+++ MVC5Course/Controllers/EFController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVC5Course.Models;
+
+namespace MVC5Course.Controllers
+{
+    public class EFController : Controller
+    {
+        FabricsEntities db = new FabricsEntities();
+        // GET: EF
+        public ActionResult Index()
+        {
+            var data = db.Product.Where(p => p.ProductName.Contains("White"));
+            return View(data);
+        }
+
+        public ActionResult Create()
+        {
+            var client = new Product()
+            {
+                ProductName = "White Cat",
+                Active = true,
+                Price=10,
+                Stock=2
+            };
+            db.Product.Add(client);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Details(int? id)
+        {
+            var product = db.Product.Find(id);
+            return View(product);
+        }
+
+        public ActionResult Delete(int? id)
+        {
+            var product = db.Product.Find(id);
+            db.Product.Remove(product);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
