b76f8f89a49528bcffd33f52b5ea88be0a9f699a
ycwu <ycwu@NB-00021>
2016-10-29 15:38:53 +0800

    43 練習複雜模型繫結並可一次傳入多筆複雜模型資料繫結到陣列裡

---
+++ MVC5Course/Controllers/MBController.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVC5Course.Models.ViewModels;
+
+namespace MVC5Course.Controllers
+{
+    public class MBController : BaseController
+    {
+        // GET: MB
+        public ActionResult Index()
+        {
+            ViewData["Temp1"] = "暫時資料 Temp1";
+            var b = new ClientLoginViewModel()
+            {
+                FirstName = "Will",
+                LastName = "Huang"
+            };
+            ViewData["Temp2"] = b;      
+            ViewBag.Temp3 = b;
+
+            return View();
+        }
+
+        public ActionResult MyForm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult MyForm(ClientLoginViewModel c)
+        {
+            if (ModelState.IsValid)
+            {
+                return RedirectToAction("MyFormResult");
+            }
+            return View();
+        }
+
+        public ActionResult MyFormResult()
+        {
+            return View();
+        }
+
+        public ActionResult ProductList()
+        {
+            var data = db.Product.OrderBy(p => p.ProductId).Take(10);
+            return View(data);
+        }
+
+        public ActionResult BatchUpdate(ProductBatchUpdateViewModel[] items)
+        {
+            if (ModelState.IsValid)
+            {
+                foreach (var item in items)
+                {
+                    var product = db.Product.Find(item.ProductId);
+                    product.ProductName = item.ProductName;
+                    product.Price = item.Price;
+                    product.Active = item.Active;
+                    product.Stock = item.Stock;
+                }
+                db.SaveChanges();
+            }
+            return RedirectToAction("ProductList");
+        }
+    }
+}
---
+++ MVC5Course/Models/ViewModels/ProductBatchUpdateViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC5Course.Models.ViewModels
+{
+    public class ProductBatchUpdateViewModel
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public Nullable<decimal> Price { get; set; }
+        public Nullable<bool> Active { get; set; }
+        public Nullable<decimal> Stock { get; set; }
+    }
+}
