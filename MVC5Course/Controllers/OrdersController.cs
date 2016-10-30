74443efbbe5d6496da96e585380c7452575452b5
ycwu <ycwu@NB-00021>
2016-10-30 15:13:23 +0800

    50 練習使用 Html.Partial 與 Html.Action 方法載入其他頁面

---
+++ MVC5Course/Controllers/OrdersController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using MVC5Course.Models;
+
+namespace MVC5Course.Controllers
+{
+    public class OrdersController : Controller
+    {
+        private FabricsEntities db = new FabricsEntities();
+
+        // GET: Orders
+        public ActionResult Index(int ClientId)
+        {
+            var order = db.Order.Include(o => o.Client).Where(p => p.ClientId == ClientId);
+            return View(order.ToList());
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
